05d6056bc98994a0b3148b4ed8a05d1376664ecd
Manayek <manayekerebn7@gmail.com>
2022-11-29 09:28:19 -0800

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    if alph != 101 and alph != 113:
-        print("{:c}".format(alph), end="")
+if alph != 101 and alph != 113:
+print("{:c}".format(alph), end="")
+
 
