4899cc3107e02c8b5f7e464770d0100b30de2972
Manayek <manayekerebn7@gmail.com>
2022-11-29 11:13:33 -0800

    task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-         i = i - 32
-     else:
-         i = i
-         print("{}".format(chr(i)), end="")
+for a in reversed(range(ord("a"), ord("z") + 1)):
+    if a % 2 != 0:
+        a = a - 32
+        print("{:c}".format(a), end="")
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if (i % 3 == 0) and (i % 5 == 0):
-            i = "FizzBuzz"
-        elif i % 3 == 0:
-            i = "Fizz"
-        elif i % 5 == 0:
-            i = "Buzz"
-            print("{} ".format(i), end="")
+for a in reversed(range(ord("a"), ord("z") + 1)):
+    if a % 2 != 0:
+        a = a - 32
+        print("{:c}".format(a), end="")
