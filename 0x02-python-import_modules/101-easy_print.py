bbc29537d0a06da8cdd6bf8745fd14907200fe7c
Manayek <manayekerebn7@gmail.com>
2022-12-01 23:33:56 -0800

    task9

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
-
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
