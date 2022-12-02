38d1ecbef96b1031ad44db8766d23ce2e10db244
Manayek <manayekerebn7@gmail.com>
2022-12-01 23:23:43 -0800

    task8

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
+# 101-remove_char_at.py
 
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-            return (c)
-
-        else:
-            return(sub(a, b))
+def remove_char_at(str, n):
+    """Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
