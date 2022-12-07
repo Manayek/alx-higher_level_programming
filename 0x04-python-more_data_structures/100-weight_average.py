0c13281e8322dd4d868cff9225c637189a412595
Manayek <manayekerebn7@gmail.com>
2022-12-07 09:41:43 -0800

    task13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-
-# weight_average - returns the weighted average of all intergers tuple.
 def weight_average(my_list=[]):
-    if my_list:
-        weight = 0
-        denom = 0
-        avg = 0
-        #rint(my_list[0][0])
-        for i in range(len(my_list)):
-            for j in range(len(my_list[i])):
-                weight += my_list[i][0] * my_list[i][1]
-                denom += my_list[i][1]
-                avg = weight / denom
-                return avg
-            return 0
+    if not my_list:
+        return 0
+    listix = []
+    listy = []
+    for x, j in my_list:
+        listix.append(x*j)
+        for x, j in my_list:
+            listy.append(j)
+            return(sum(listix)/sum(listy))
+
+
+        #second method :
+        #liste comprehension : 
+        #somme = sum([x*y for x, y in my_list])
+        #return(somme/sum([y for x, y in my_list])
+
+        #Third method :
+        #Direct additions using Tuples :
+        #     somme1, somme2 = 0
+        #     for tulpe in my_list:
+        #     somme1 +=(tulpe[0] * tulp[1])
+        #     somme2 += tulp[1]
+        #     somme1/somme2
