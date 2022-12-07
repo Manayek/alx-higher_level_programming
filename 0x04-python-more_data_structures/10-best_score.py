5bb2393d4a0f7b5deeb5aec0537bf1b6bab07b56
Manayek <manayekerebn7@gmail.com>
2022-12-07 09:34:34 -0800

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
-# best_score - retuns a key with the biggest integer value
 def best_score(a_dictionary):
-    if a_dictionary:
-        dlist = list(a_dictionary)
-        largK = dlist[0]
-        for i in dlist:
-            if a_dictionary[largK] < a_dictionary[i]:
-                largK = i
-                return largK
-            else:
-                return
+    if not a_dictionary:
+        return None
+    max_value = max(a_dictionary.values())
+    for key in a_dictionary.keys():
+        if a_dictionary[key] == max_value:
+            return 
