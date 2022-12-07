ba3f014d4b343769371c1b102a68828f2f6c59ce
Manayek <manayekerebn7@gmail.com>
2022-12-07 09:57:10 -0800

    task15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    schl = []
-    for key, valu in a_dictionary.items():
-        if valu == value:
-            schl.append(key)
-            for key in schl:
-                del a_dictionary[key]
-                return(a_dictionary)
+def complex_delete(my_dict, value):
+    targets = []
+    for key, key_value in my_dict.items():
+        if key_value is value:
+            targets.append(key)
+            for x in targets:
+                del my_dict[x]
+                return(my_dict)
