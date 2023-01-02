1ccdfa05c158b8d95cb12838e11c62c3bc822cd9
Manayek <manayekerebn7@gmail.com>
2023-01-02 09:50:58 -0800

    python more classess

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,72 +1,52 @@
 #!/usr/bin/python3
-'''class that defines new Rectangle '''
-
-
 class Rectangle:
-    '''new Class'''
-
     def __init__(self, width=0, height=0):
-
+        if not isinstance(width, int) or not isinstance(height, int):
+            wrong = "width" if not isinstance(width, int) else "height"
+            raise TypeError(wrong + " must be an integer")
+        if height < 0 or width < 0:
+            wrong = "width" if width < 0 else "height"
+            raise ValueError(wrong + " must be >= 0")
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            ''' a property to retrieve the value of width '''
-            return(self.__width)
-
-        @width.setter
-        def width(self, value):
-            ''' property setter to set the value of width '''
-
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
+        def __str__(self):
+            if self.__width > 0 and self.__height > 0:
+                return ("\n".join(["".join(["#"
+                    for y in range(self.__width)])
+                    for x in range(self.__height)]))
             else:
+                return ("")
+
+            @property
+            def width(self):
+                return(self.__width)
+
+            @width.setter
+            def width(self, value):
+                if not isinstance(value, int):
+                    raise TypeError("width must be an integer")
+                if value < 0:
+                    raise ValueError("width must be >= 0")
                 self.__width = value
 
                 @property
                 def height(self):
-
-                    return self.__height
+                    return(self.__height)
 
                 @height.setter
                 def height(self, value):
-                    ''' propoerty setter to set the value of hight '''
-
                     if not isinstance(value, int):
                         raise TypeError("height must be an integer")
-                    elif value < 0:
+                    if value < 0:
                         raise ValueError("height must be >= 0")
-                    else:
-                        self.__height = value
+                    self.__height = value
 
-                        def area(self):
-                            ''' Public method to compute the area'''
+                    def area(self):
+                        return(self.__width * self.__height)
 
-                            return self.__height * self.__width
-
-                        def perimeter(self):
-                            '''public method to compute the perimeter'''
-
-                            if self.__height == 0 or self.__width == 0:
-                                return 0
-
-                            else:
-                                return self.__height * 2 + self.__width * 2
-
-                            def __str__(self):
-                                ''' print the rectange in formal way '''
-
-                                str = ''
-
-                                if self.__width is 0 or self.__height is 0:
-                                    return str
-                                for i in range(self.__height):
-                                    for j in range(self.__width):
-                                        str += '#'
-                                        str += '\n'
-                                        str = str[0:-1]
-
-                                        return str
+                    def perimeter(self):
+                        if self.__width > 0 and self.__height > 0:
+                            return(self.__width * 2 + self.__height * 2)
+                        else:
+                            return (0)
