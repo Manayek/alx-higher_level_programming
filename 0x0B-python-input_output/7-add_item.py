180e9373a8112e19f8022777f662006020a2c3e0
Manayek <111649457+Manayek@users.noreply.github.com>
2023-01-10 19:52:12 +0300

    Create 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
-import json
-import os.path
+"""
+Python function that add arguments to a Python list
+"""
 import sys
-from sys import argv
+import os
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-json_list = []
+my_list = []
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
+if not os.path.exists("./add_item.json"):
+    save_json(my_list, "add_item.json")
+my_list = load_json("add_item.json")
 
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+for items in sys.argv[1:]:
+    my_list.append(items)
+save_json(my_list, "add_item.json")
