48b8e01739756b7f9b7035b6930e119e0c77dfe7
Manayek <111649457+Manayek@users.noreply.github.com>
2023-01-10 15:03:48 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-    This is a python script that adds all arguments to a Python List.
-    List is then saved to a file.
-"""
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""A script that adds all arguments to a Python list, and then save them
+to a file."""
 
+from sys import argv
+from os import path
 
-if __name__ == "__main__":
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+filename = 'add_item.json'
+if path.isfile(filename):
+    new_list = load_from_json_file(filename)
+else:
+    new_list = []
+for i in range(1, len(argv)):
+    new_list.append(argv[i])
+save_to_json_file(new_list, filename)
