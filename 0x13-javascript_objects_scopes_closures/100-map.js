f59adfc68ae6202ff6472428a06444383e3c9128
Manayek <manayekerebn7@gmail.com>
2023-03-14 10:12:57 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const arr = require('./100-data').list;
-
-console.log(arr);
-console.log(arr.map((x, idx) => x * idx));
+const list = require('./100-data.js').list;
+console.log(list);
+const newList = list.map((x, index) => x * index);
+console.log(newList);
