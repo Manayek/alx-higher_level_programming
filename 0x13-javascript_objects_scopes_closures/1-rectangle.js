c9de07ff4efd93c51b82fc700273d936767e5322
Manayek <manayekerebn7@gmail.com>
2023-03-14 10:07:13 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-// JS script to define a class and initialize them
-class Rectangle {
+/* define a class */
+module.exports = class Rectangle {
 constructor (w, h) {
 this.width = w;
 this.height = h;
 }
-}
-module.exports = Rectangle;
+};
