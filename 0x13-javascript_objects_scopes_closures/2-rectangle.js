f3b1afcc1a85d7f8e3fce60cfd049ed6ade5954c
Manayek <manayekerebn7@gmail.com>
2023-03-14 10:10:20 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
+/* define a class */
+module.exports = class Rectangle {
 constructor (w, h) {
-if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) {
-return this;
-}
+if (w > 0 && h > 0) {
 this.width = w;
 this.height = h;
 }
 }
-
-module.exports = Rectangle;
+};
