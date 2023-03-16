aa49e25d8f6564fc37af0cb27632d36a3a46b1e7
Manayek <manayekerebn7@gmail.com>
2023-03-16 04:33:05 -0700

    javascript

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process');
+// prints the addition of 2 integers
 
-/**
-* add - adds two numbers
-* Return: sum of two numbers
-*
-/
 function add (a, b) {
-a = parseInt(a);
-b = parseInt(b);
-return (a + b);
+  return parseInt(a) + parseInt(b);
 }
 
-console.log(add(argv[2], argv[3]));
+console.log(add(process.argv[2], process.argv[3]));
