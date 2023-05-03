423de0d95d961ea283bc440bf6008c7632637fb8
Manayek <111649457+Manayek@users.noreply.github.com>
2023-05-03 14:11:33 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+
+
 class Square:
-    """Type class square"""
+    """A square class"""
+    __size = None
+
     def __init__(self, size=0):
-        """Init the square classs
-        Args:
-        param1: size is the type int attribute to make it private
-        """
-        if not isinstance(size, int):
-            raise TypeError("size mut be an integer")
-        elif size < 0:
+        """Initialize class"""
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Return the area of the square"""
-        return (self.__size * self.__size)
+        """Returns the current square area"""
+        return (self.__size ** 2)
